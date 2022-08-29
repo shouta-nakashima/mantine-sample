f1e05e1ef9bf28c54796eb3fef02a79ebffc91c5
nakashima-dh <snakashima@dhsoc.jp>
2022-08-29 17:03:04 +0900

    project setup

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import React,{FC,ReactNode} from 'react';
+import Head from "next/head";
+type Props = {
+  title:string
+  children:ReactNode
+}
+
+const Layout:FC<Props> = ({children,title='Mantine'}) => {
+  return (
+    <div className='flex min-h-screen'>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header></header>
+      <main className='flex flex-1 flex-col justify-center p-4'>
+        {children}
+      </main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NextPage} from "next";
+import Layout from "../components/Layout";
+
+const ButtonDemo:NextPage = () => {
+  return (
+    <Layout title={'buttons'}>
+      Button
+    </Layout>
+  );
+};
+
+export default ButtonDemo;
